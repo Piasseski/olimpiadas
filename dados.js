599302c991c5b4f346cb64e7cc64fef499f1a48a
Piasseski <119076721+Piasseski@users.noreply.github.com>
2024-09-06 17:12:20 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, conhecida como a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        tags="skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou o mundo com suas técnicas e determinação.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+        tags="judô judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com sua força e elegância.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+        tags="ginástica ginastica ouro"
+    }
+];
